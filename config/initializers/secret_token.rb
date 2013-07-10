5c5ff97938036df766f6163a01dcb338aaeec294
CaptConrado <ottey001@gmail.com>
2013-07-09 20:31:16 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catchupsports::Application.config.secret_token = 'fd4112ede8a9f897787c34e14cfd8c1497e704e04db49683ee8fd83ae4c816d46debd5a6dfff9d4916f0e96e827e94c74c08c9f812b2ff89d5c3c88fc3e4ffaf'
